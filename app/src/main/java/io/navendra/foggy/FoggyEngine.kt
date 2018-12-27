2ec983794e64ab3065481f935df83be2319f68ad
Navendra Jha <navendra@asu.edu>
2018-12-27 15:50:41 -0700

    Added loadurl functionality

--- app/src/main/java/io/navendra/foggy/FoggyEngine.kt
+++ app/src/main/java/io/navendra/foggy/FoggyEngine.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import mozilla.components.browser.engine.gecko.GeckoEngine
 import mozilla.components.concept.engine.DefaultSettings
 import mozilla.components.concept.engine.EngineSession
-import org.mozilla.geckoview.GeckoRuntime
 
 class FoggyEngine(context: Context){
 
@@ -14,7 +13,6 @@ class FoggyEngine(context: Context){
 
     // Create an engine instance to be used by other components.
     val gecko by lazy { GeckoEngine(context, defaultSettings)  }
-
 
 
 
--- app/src/main/java/io/navendra/foggy/ui/MainActivity.kt
+++ app/src/main/java/io/navendra/foggy/ui/MainActivity.kt
@@ -6,34 +6,60 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import io.navendra.foggy.App
-import io.navendra.foggy.FoggyEngine
 import io.navendra.foggy.R
 import kotlinx.android.synthetic.main.activity_main.*
-import mozilla.components.browser.engine.gecko.GeckoEngine
+import mozilla.components.browser.session.Session
+import mozilla.components.browser.session.SessionManager
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.engine.EngineSession
 import mozilla.components.concept.engine.EngineView
+import mozilla.components.feature.session.SessionFeature
+import mozilla.components.feature.session.SessionUseCases
+import mozilla.components.feature.toolbar.ToolbarFeature
 
 class MainActivity : AppCompatActivity() {
 
     private val engine: Engine by lazy { (application as App).engine  }
-    private lateinit var engineView: EngineView
+
     private lateinit var session: EngineSession
+    private lateinit var toolbarFeature: ToolbarFeature
+    private lateinit var sessionFeature: SessionFeature
+    private lateinit var sessionUseCases: SessionUseCases
+    private lateinit var sessionManager: SessionManager
 
     private val HOME_URL = "https://www.google.com/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        session = engine.createSession()
+        sessionManager = SessionManager(engine)
+        sessionUseCases = SessionUseCases(sessionManager)
 
-        session.loadUrl(HOME_URL)
+        sessionFeature = SessionFeature(
+            sessionManager,
+            sessionUseCases,
+            conceptEngineView
+        )
+        toolbarFeature = ToolbarFeature(
+            toolbar,
+            sessionManager,
+            sessionUseCases.loadUrl
+        )
 
-        engineView = conceptEngineView.apply {
-            render(session)
-        }
+        //Adding a default session
+        sessionManager.add(Session("https://www.mozilla.org"))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        toolbarFeature.start()
+        sessionFeature.start()
+    }
+    override fun onStop() {
+        super.onStop()
+        toolbarFeature.stop()
+        sessionFeature.stop()
     }
 
     override fun onCreateView(name: String?, context: Context?,
